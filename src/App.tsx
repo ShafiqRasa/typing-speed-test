2f8c0d9c2cab07a4b1af870e4763c4c38d923917
Shafiq Salehi <rasashafiq@gmail.com>
2026-05-06 11:56:22 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="container">
+        <h1>Typing Speed Test Web Application</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis,
+          quae qui officiis magni voluptas praesentium tempora voluptatibus at,
+          in consequatur non accusantium dolore tempore excepturi dolorum natus
+          quas neque voluptatem.
+        </p>
+        <button className="button">Get Started</button>
+        <a href="#">Get Started</a>
+      </section>
+    </>
+  );
+}
+
+export default App;
