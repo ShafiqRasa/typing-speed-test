45c384f2af11a7705562f955d6565fde5f8831d8
Shafiq Salehi <rasashafiq@gmail.com>
2026-05-06 12:31:50 +0200

    initial design system are done, and index.css is almost ready  for the starting the layout design

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ function App() {
   return (
     <>
       <section className="container">
-        <h1>Typing Speed Test Web Application</h1>
+        <h1> Typing Speed Test Web Application</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis,
           quae qui officiis magni voluptas praesentium tempora voluptatibus at,
           in consequatur non accusantium dolore tempore excepturi dolorum natus
           quas neque voluptatem.
         </p>
-        <button className="button">Get Started</button>
+        <button>Get Started</button>
         <a href="#">Get Started</a>
       </section>
     </>
