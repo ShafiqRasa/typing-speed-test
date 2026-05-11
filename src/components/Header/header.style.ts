0ea3db45a2152d74da1d9ab5fd5cd9af7dd8f107
Shafiq Salehi <rasashafiq@gmail.com>
2026-05-11 18:41:33 +0200

    done

--- src/components/Header/header.style.ts
+++ src/components/Header/header.style.ts
@@ -5,6 +5,14 @@ export const Header = styled.header`
   justify-content: space-between;
   align-items: center;
 
+  .logo {
+    width: 32px;
+    height: 32px;
+    background-image: url("/src/assets/images/logo-small.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+
   .best-wpm {
     display: flex;
     align-items: center;
@@ -13,5 +21,13 @@ export const Header = styled.header`
 
   span {
     color: gray;
+  }
+
+  @media screen and (min-width: 768px) {
+    .logo {
+      width: 267px;
+      height: 40px;
+      background-image: url("/src/assets/images/logo-large.svg");
+    }
   }
 `;
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,7 +3,8 @@ import { Header } from "./header.style";
 const NavBar = () => {
   return (
     <Header>
-      <img src="/src/assets/images/logo-small.svg" alt="logo" />
+      {/* <img src="/src/assets/images/logo-small.svg" alt="logo" /> */}
+      <div className="logo" />
       <div className="best-wpm">
         <img src="/src/assets/images/icon-personal-best.svg" alt="trophy" />
         <div>
