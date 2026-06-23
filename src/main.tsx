f659c32f1c9dc8216e070c2f91fcd382400bf0bf
Shafiq Salehi <rasashafiq@gmail.com>
2026-06-23 10:33:24 +0200

    installed react router dom npm package, and laveraged the Outlet from react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,24 @@ import "./index.scss";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <div>content</div>,
+      },
+    ],
+  },
+]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 );
